5314bc77c29953560e2180739b5946af4c0401b5
Bo Bayles <bbayles@obsrvbl.com>
2018-11-02 14:40:25 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gcp_flowlogs_reader',
-    version='0.1',
+    version='0.2',
     license='Apache',
     url='https://github.com/obsrvbl/gcp-flowlogs-reader',
 
