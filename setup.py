60b03eef8d17b317d8bc4012b34d3a60bcaf0724
Bo Bayles <bbayles@gmail.com>
2021-01-11 09:28:02 -0600

    Fix GCP Log client versions

--- setup.py
+++ setup.py
@@ -18,10 +18,10 @@ setup(
         'Intended Audience :: Information Technology',
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     entry_points={
         'console_scripts': [
@@ -31,7 +31,7 @@ setup(
     packages=find_packages(exclude=[]),
     test_suite='tests',
     install_requires=[
-        'google-cloud-logging>=1.6.0',
+        'google-cloud-logging>=1.6.0,<2.0.0',
         'google-cloud-resource-manager>=0.28.3',
     ],
     tests_require=[],
